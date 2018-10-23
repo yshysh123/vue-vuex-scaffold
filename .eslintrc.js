e6461394193e87ca0bd8779355929c90889097b7
yusihong1 <yucaihong@jd.com>
2018-10-23 17:20:56 +0800

    增加eslint规则集合prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+/** @format */
+
 module.exports = {
   root: true,
   env: {
@@ -6,12 +8,26 @@ module.exports = {
   extends: [
     'plugin:vue/essential',
     '@vue/airbnb',
+    'plugin:prettier/recommended',
+    'prettier',
   ],
   rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: false,
+        trailingComma: 'all',
+        bracketSpacing: true,
+        jsxBracketSameLine: true,
+        parser: 'flow',
+      },
+    ],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "no-alert": process.env.NODE_ENV === "production" ? "error" : "off",
   },
   parserOptions: {
     parser: 'babel-eslint',
   },
-};
+}
--- README.md
+++ README.md
@@ -1,36 +1,43 @@
 # my-project
 
 ## Project setup
+
 ```
 yarn install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
-yarn run serve
+yarn run start
 ```
 
 ### Compiles and minifies for production
+
 ```
 yarn run build
 ```
 
 ### Run your tests
+
 ```
 yarn run test
 ```
 
 ### Lints and fixes files
+
 ```
 yarn run lint
 ```
 
 ### Run your end-to-end tests
+
 ```
 yarn run test:e2e
 ```
 
 ### Run your unit tests
+
 ```
 yarn run test:unit
 ```
