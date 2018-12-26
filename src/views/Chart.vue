f89b65aac7e24652e200e3db704f9800a998bf2d
yusihong1 <yucaihong@jd.com>
2018-12-26 09:18:50 +0800

    feat(add more charts): 增加多个图表样例

--- src/views/Chart.vue
+++ src/views/Chart.vue
@@ -1,8 +1,29 @@
 <template>
   <div class="Chart">
-    <vCharts :options="options"
-      style="width:100%;height:400px"
-      autoResize=true />
+    <el-row :gutter="20">
+      <el-col :span="12">
+        <vCharts :options="options"
+          style="width:100%;height:400px"
+          autoResize=true />
+      </el-col>
+      <el-col :span="12">
+        <vCharts :options="options2"
+          style="width:100%;height:400px"
+          autoResize=true />
+      </el-col>
+    </el-row>
+    <el-row :gutter="20">
+      <el-col :span="12">
+        <vCharts :options="options3"
+          style="width:100%;height:400px"
+          autoResize=true />
+      </el-col>
+      <el-col :span="12">
+        <vCharts :options="options4"
+          style="width:100%;height:400px"
+          autoResize=true />
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -26,6 +47,203 @@ export default {
           },
         ],
       },
+      options2: {
+        title: {
+          text: '某地区蒸发量和降水量',
+          subtext: '纯属虚构',
+        },
+        tooltip: {
+          trigger: 'axis',
+        },
+        legend: {
+          data: ['蒸发量', '降水量'],
+        },
+        toolbox: {
+          show: true,
+          feature: {
+            dataView: { show: true, readOnly: false },
+            magicType: { show: true, type: ['line', 'bar'] },
+            restore: { show: true },
+            saveAsImage: { show: true },
+          },
+        },
+        calculable: true,
+        xAxis: [
+          {
+            type: 'category',
+            data: [
+              '1月',
+              '2月',
+              '3月',
+              '4月',
+              '5月',
+              '6月',
+              '7月',
+              '8月',
+              '9月',
+              '10月',
+              '11月',
+              '12月',
+            ],
+          },
+        ],
+        yAxis: [
+          {
+            type: 'value',
+          },
+        ],
+        series: [
+          {
+            name: '蒸发量',
+            type: 'bar',
+            data: [
+              2.0,
+              4.9,
+              7.0,
+              23.2,
+              25.6,
+              76.7,
+              135.6,
+              162.2,
+              32.6,
+              20.0,
+              6.4,
+              3.3,
+            ],
+            markPoint: {
+              data: [
+                { type: 'max', name: '最大值' },
+                { type: 'min', name: '最小值' },
+              ],
+            },
+            markLine: {
+              data: [{ type: 'average', name: '平均值' }],
+            },
+          },
+          {
+            name: '降水量',
+            type: 'bar',
+            data: [
+              2.6,
+              5.9,
+              9.0,
+              26.4,
+              28.7,
+              70.7,
+              175.6,
+              182.2,
+              48.7,
+              18.8,
+              6.0,
+              2.3,
+            ],
+            markPoint: {
+              data: [
+                { name: '年最高', value: 182.2, xAxis: 7, yAxis: 183 },
+                { name: '年最低', value: 2.3, xAxis: 11, yAxis: 3 },
+              ],
+            },
+            markLine: {
+              data: [{ type: 'average', name: '平均值' }],
+            },
+          },
+        ],
+      },
+      options3: {
+        title: {
+          text: 'Customized Pie',
+          left: 'center',
+          top: 20,
+          textStyle: {
+            color: '#ccc',
+          },
+        },
+
+        tooltip: {
+          trigger: 'item',
+          formatter: '{a} <br/>{b} : {c} ({d}%)',
+        },
+
+        visualMap: {
+          show: false,
+          min: 80,
+          max: 600,
+          inRange: {
+            colorLightness: [0, 1],
+          },
+        },
+        series: [
+          {
+            name: '访问来源',
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '50%'],
+            data: [
+              { value: 335, name: '直接访问' },
+              { value: 310, name: '邮件营销' },
+              { value: 274, name: '联盟广告' },
+              { value: 235, name: '视频广告' },
+              { value: 400, name: '搜索引擎' },
+            ].sort(function(a, b) {
+              return a.value - b.value
+            }),
+            roseType: 'radius',
+            label: {
+              normal: {
+                textStyle: {
+                  color: 'rgba(255, 255, 255, 0.3)',
+                },
+              },
+            },
+            labelLine: {
+              normal: {
+                lineStyle: {
+                  color: 'rgba(255, 255, 255, 0.3)',
+                },
+                smooth: 0.2,
+                length: 10,
+                length2: 20,
+              },
+            },
+            itemStyle: {
+              normal: {
+                color: '#c23531',
+                shadowBlur: 200,
+                shadowColor: 'rgba(0, 0, 0, 0.5)',
+              },
+            },
+
+            animationType: 'scale',
+            animationEasing: 'elasticOut',
+            animationDelay: function(idx) {
+              return Math.random() * 200
+            },
+          },
+        ],
+      },
+      options4: {
+        xAxis: {},
+        yAxis: {},
+        series: [
+          {
+            symbolSize: 20,
+            data: [
+              [10.0, 8.04],
+              [8.0, 6.95],
+              [13.0, 7.58],
+              [9.0, 8.81],
+              [11.0, 8.33],
+              [14.0, 9.96],
+              [6.0, 7.24],
+              [4.0, 4.26],
+              [12.0, 10.84],
+              [7.0, 4.82],
+              [5.0, 5.68],
+            ],
+            type: 'scatter',
+          },
+        ],
+      },
     }
   },
   computed: {},
@@ -34,5 +252,5 @@ export default {
 }
 </script>
 
-<style lang="less" scoped="" type="text/css">
+<style lang="less" scoped type="text/css">
 </style>
