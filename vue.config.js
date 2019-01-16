69de26c45eafb36114e7f40e6eacee952274a6a8
yusihong1 <yucaihong@jd.com>
2019-01-16 16:00:43 +0800

    build(:wrench: add gzip and production mode): 增加gzip

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,12 @@
 const devServerConfig = require('./build/webpackResolve')
 const path = require('path')
 const SkeletonWebpackPlugin = require('vue-skeleton-webpack-plugin')
-// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
-
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+// 是否使用gzip
+const productionGzip = true
+// 需要gzip压缩的文件后缀
+const productionGzipExtensions = ['js', 'css']
 module.exports = {
   css: {
     // 打开开发阶段有骨架屏样式，但是所有样式无法热加载；关闭开发阶段无法加载骨架屏样式
@@ -12,10 +16,24 @@ module.exports = {
     /\/node_modules\/vue-echarts\//,
     /\/node_modules\/resize-detector\//,
   ],
-  baseUrl: process.env.NODE_ENV === 'production' ? './' : '',
-  configureWebpack: {
-    // plugins: [new BundleAnalyzerPlugin()],
-    plugins: [
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '',
+  productionSourceMap: process.env.NODE_ENV !== 'production',
+  configureWebpack: () => {
+    const myConfig = {}
+    myConfig.plugins = []
+    if (process.env.NODE_ENV === 'production') {
+      myConfig.plugins.push(new BundleAnalyzerPlugin())
+      if (productionGzip) {
+        myConfig.plugins.push(
+          new CompressionWebpackPlugin({
+            test: new RegExp(`\\.(${productionGzipExtensions.join('|')})$`),
+            threshold: 8192,
+            minRatio: 0.8,
+          }),
+        )
+      }
+    }
+    myConfig.plugins.push(
       new SkeletonWebpackPlugin({
         webpackConfig: {
           entry: {
@@ -42,28 +60,31 @@ module.exports = {
           ],
         },
       }),
-    ],
-    devServer: {
-      historyApiFallback: true,
-      disableHostCheck: true,
-      host: '0.0.0.0',
-      inline: true,
-      port: 8080,
-      hot: true,
-      contentBase: './public',
-      proxy: {
-        '/api/': {
-          target: 'http://localhost:8089',
-          pathRewrite: {
-            '^/api/': '',
+    )
+    if (process.env.NODE_ENV === 'development') {
+      myConfig.devServer = {
+        historyApiFallback: true,
+        disableHostCheck: true,
+        host: '0.0.0.0',
+        inline: true,
+        port: 8080,
+        hot: true,
+        contentBase: './public',
+        proxy: {
+          '/api/': {
+            target: 'http://localhost:8089',
+            pathRewrite: {
+              '^/api/': '',
+            },
+            secure: false,
           },
-          secure: false,
         },
-      },
-      before() {},
-    },
-    resolve: {
+        before() {},
+      }
+    }
+    myConfig.resolve = {
       ...devServerConfig,
-    },
+    }
+    return myConfig
   },
 }
