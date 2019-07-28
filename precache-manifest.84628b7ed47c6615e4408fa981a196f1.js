6800d0ae903cc04d2e5a84000acb68fee8b5ce3e
yush <389636851@qq.com>
2019-07-28 02:28:49 +0000

    Update tools

---
+++ precache-manifest.84628b7ed47c6615e4408fa981a196f1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "url": "static/js/1564280863841-user.a49f01e4ccc9ff458281.js"
+  },
+  {
+    "url": "static/js/1564280863841-app.ce20617a938f695a048a.js"
+  },
+  {
+    "url": "static/js/1564280863841-chunk-vendors.f9a7c0aa0f166006a169.js"
+  },
+  {
+    "url": "static/js/1564280863841-fetch.c0082d5fa875f40f2ede.js"
+  },
+  {
+    "url": "static/js/1564280863841-chart.ea44d8d0603f82ee7064.js"
+  },
+  {
+    "url": "static/js/1564280863841-main.601e300419c35e6b1f3c.js"
+  },
+  {
+    "url": "static/js/1564280863841-main~polyfill.c71e00efa2c11948229e.js"
+  },
+  {
+    "url": "static/js/1564280863841-polyfill.af66798642eb74c2f437.js"
+  },
+  {
+    "revision": "601e300419c35e6b1f3c",
+    "url": "static/css/1564280863841-main.0de56678.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "3b6cc82353e241264d3a9a86cf60c16c",
+    "url": "index.html"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "59d46b62f9306abcdeb3220a3730d1d1",
+    "url": "asset/media/success.m4a"
+  },
+  {
+    "revision": "20f0c00ddb9260a9f7c399f8bdf2fa9b",
+    "url": "asset/media/failure.m4a"
+  },
+  {
+    "revision": "0a3351a722ed24bd13f943907c89c06d",
+    "url": "asset/images/Groritannien.png"
+  },
+  {
+    "revision": "d086882d49953c85dcca01713df1edb3",
+    "url": "asset/images/China.png"
+  }
+];
